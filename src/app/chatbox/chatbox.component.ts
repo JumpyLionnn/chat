b26dc99e58865010e6d7038c6657730efae9b33c
JumpyLionnn <fwipsity@gmail.com>
2022-01-06 12:15:22 +0200

    changed the send button

--- src/app/chatbox/chatbox.component.ts
+++ src/app/chatbox/chatbox.component.ts
@@ -7,8 +7,8 @@ import { Message } from './chatmessage/message.model';
   styleUrls: ['./chatbox.component.css']
 })
 export class ChatboxComponent implements OnInit {
-  public allowSendingMessages: boolean = true;
-  public messageInputContent: string = "";
+  public allowSendingMessages: boolean = false;
+  public messageContentInput: string = "";
   public nameInputContent: string = "";
   public messages: Message[] = [];
   public alerts: {type: string, content: string}[] = [];
@@ -19,7 +19,7 @@ export class ChatboxComponent implements OnInit {
   }
 
   public async onSendMessage(){
-    const content = this.messageInputContent.trim(); 
+    const content = this.messageContentInput.trim(); 
     const username = this.nameInputContent.trim(); 
     if(content === ""){
       this.addAlert("error", "You can not send an empty message.");
@@ -36,7 +36,28 @@ export class ChatboxComponent implements OnInit {
     const time = (hours > 12 ? hours - 12 : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + (hours > 11 ? " PM" : " AM");
 
     this.messages.push(new Message(username, content, time));
-    this.messageInputContent = "";
+    this.messageContentInput = "";
+    this.allowSendingMessages = false;
+  }
+
+  public onMessageContentInputChange(event: Event){
+    this.messageContentInput = (<HTMLInputElement>event.target).value;
+    if(this.messageContentInput.trim() === ""){
+      this.allowSendingMessages = false;
+    }
+    else{
+      this.allowSendingMessages = true;
+    }
+  }
+
+  public onInputKeyDown(event: KeyboardEvent){
+    if(event.key === "Enter" && !event.repeat){
+      this.onSendMessage();
+    }
+  }
+
+  public onReplyClick(event){
+    this.messageContentInput = `@${event.username} `;
   }
 
   public addAlert(type: string, content: string, time: number = 5000){
@@ -47,16 +68,6 @@ export class ChatboxComponent implements OnInit {
     setTimeout(()=>{
       this.alerts.splice(this.alerts.indexOf(alert), 1);
     }, time);
-  }
-
-  public onInputKeyDown(event: KeyboardEvent){
-    if(event.key === "Enter" && !event.repeat){
-      this.onSendMessage();
-    }
-  }
-
-  public onReplyClick(event){
-    this.messageInputContent = `@${event.username} `;
   }
 
   public onAlertClose(index: number){
