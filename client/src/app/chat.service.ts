51a2eda64f1dfed094b0ea4ac2a9cfbb2f1db777
JumpyLionnn <fwipsity@gmail.com>
2022-01-09 23:06:05 +0200

    connected firebase

--- client/src/app/chat.service.ts
+++ client/src/app/chat.service.ts
@@ -1,11 +1,12 @@
 import { Observable } from 'rxjs';
 import { io, Socket } from "socket.io-client";
+import { environment } from 'src/environments/environment';
 
 
 
 export class ChatService {
   public message: Observable<{username:string,content:string}>;
-  private socket: Socket = io('http://localhost:3000');
+  private socket: Socket = io(environment.chatHost);
 
   constructor() {
     this.message = Observable.create((observer)=>{
--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  chatHost: "https://basic-simple-chat.herokuapp.com/"
 };
