7af86c23fde6da6532014ce204cd48f55a34c76d
JumpyLionnn <jumpylionn1@gmail.com>
2022-01-11 12:11:36 +0200

    added a multiline messages support

--- client/src/app/chatbox/chatbox.component.ts
+++ client/src/app/chatbox/chatbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Message } from './chatmessage/message.model';
 import { ScrollableDirective } from '../directives/scrollable/scrollable.directive';
 import { ChatService } from '../chat.service';
@@ -17,15 +17,19 @@ export class ChatboxComponent implements OnInit, OnDestroy {
   public alerts: {type: string, content: string}[] = [];
 
   private userMentionAudioId: number;
+  private readonly maxVisibleRowAmount = 6;
+  private readonly isMobile: boolean = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
   @Input()
   public username: string;
 
   @ViewChild(ScrollableDirective)
   public scrollableDirective: ScrollableDirective;
+  @ViewChild("textarea", {static: true})
+  public textareaRef: ElementRef<HTMLTextAreaElement>;
 
   @ViewChild("messagesContainer", {static: true})
-  public messagesContainerRef: ElementRef;
+  public messagesContainerRef: ElementRef<HTMLDivElement>;
   constructor(private chatService: ChatService, private audioSevice: AudioService) { 
   }
 
@@ -59,7 +63,7 @@ export class ChatboxComponent implements OnInit, OnDestroy {
     //reseting the input
     this.messageContentInput = "";
     this.allowSendingMessages = false;
-
+    this.textareaRef.nativeElement.rows = 1;
   }
 
   public addMessage(username: string, content: string){
@@ -74,21 +78,61 @@ export class ChatboxComponent implements OnInit, OnDestroy {
     this.scrollableDirective.adjustScroll();
   }
 
-  public onMessageContentInputChange(event: Event){
-    this.messageContentInput = (<HTMLInputElement>event.target).value;
+  public onMessageContentInputChange(event: InputEvent){
+    const textarea = this.textareaRef.nativeElement;
+    this.messageContentInput = textarea.value;
     if(this.messageContentInput.trim() === ""){
       this.allowSendingMessages = false;
     }
     else{
       this.allowSendingMessages = true;
     }
+
+    const lines = this.calculateLines(textarea);
+    textarea.rows = lines > this.maxVisibleRowAmount ? this.maxVisibleRowAmount : lines;
   }
 
   public onInputKeyDown(event: KeyboardEvent){
-    if(event.key === "Enter" && !event.repeat){
-      this.onSendMessage();
+    if(event.key === "Enter"){
+      const textarea = this.textareaRef.nativeElement;
+      if((event.shiftKey || this.isMobile)){
+        if(textarea.value.split("\n").length < 10){
+          const selection = textarea.selectionStart;
+          const value = textarea.value;
+          textarea.value = value.substring(0, selection) + "\n" + value.substring(selection, value.length);
+          if(this.calculateLines(textarea) < this.maxVisibleRowAmount){
+            textarea.rows++;
+          }
+          textarea.setSelectionRange(selection + 1, selection + 1);
+          }
+      }
+      else if(!event.repeat){
+        this.onSendMessage();
+      }
+      event.preventDefault();
     }
   }
+
+  @HostListener("window:resize", ["$event"])
+  public onResize(event: Event){
+    const textarea = this.textareaRef.nativeElement;
+    const lines = this.calculateLines(textarea);
+    textarea.rows = lines > this.maxVisibleRowAmount ? this.maxVisibleRowAmount : lines;
+
+  }
+
+  private calculateLines(element: HTMLTextAreaElement): number{
+    const computedStyle = window.getComputedStyle(element);
+    const fontSize = parseFloat(computedStyle.getPropertyValue('font-size'));
+    const padding = parseFloat(computedStyle.getPropertyValue('padding-top')) + 
+      parseFloat(computedStyle.getPropertyValue('padding-bottom'));
+    const rows = element.rows;
+    element.rows = 1;
+    const result = (element.scrollHeight - padding) / fontSize / 3 * 2;
+    element.rows = rows;
+    return result;
+  }
+  
 
   public onReplyClick(event){
     this.messageContentInput = `@${event.username} `;
